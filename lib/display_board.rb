1333f8c3322e7ef0d209aa8415b7680de9294f31
Dina Hafez <dinahafez.8@gmail.com>
2018-06-03 14:52:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[0]} | #{board[3]} | #{board[6]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[1]} | #{board[4]} | #{board[7]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[2]} | #{board[5]} | #{board[8]} "
 end
